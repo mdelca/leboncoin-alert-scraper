f77f6ee6e3cb7021a3daad64197322017e46377c
Marion Delcambre <delcambremarion@gmail.com>
2017-12-15 21:22:36 +0100

    replace tab by spaces

--- email_me.py
+++ email_me.py
@@ -11,27 +11,27 @@ mail_server = settings.SERVER_EMAIL_SENDER
 mail_server_port = settings.SERVER_EMAIL_PORT
 
 def send_email(subject, body):
-	from email.mime.multipart import MIMEMultipart
-	from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
 
-	msg = MIMEMultipart()
-	msg['From'] = fromaddr
-	msg['To'] = toaddr
-	msg['Subject'] = subject
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = subject
 
-	msg.attach(MIMEText(body, 'plain'))
+    msg.attach(MIMEText(body, 'plain'))
 
-	import smtplib
-	try:
-		server = smtplib.SMTP(mail_server, mail_server_port)
-		server.ehlo()
-		if password:
-			server.starttls()
-			server.ehlo()
-			server.login(fromaddr, password)
-		text = msg.as_string()
-		server.sendmail(fromaddr, toaddr, text)
-	except smtplib.socket.error:
-		print('mail socket error')
-	except smtplib.SMTPException:
-		print('mail smtp exception')
+    import smtplib
+    try:
+        server = smtplib.SMTP(mail_server, mail_server_port)
+        server.ehlo()
+        if password:
+            server.starttls()
+            server.ehlo()
+            server.login(fromaddr, password)
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+    except smtplib.socket.error:
+        print('mail socket error')
+    except smtplib.SMTPException:
+        print('mail smtp exception')
