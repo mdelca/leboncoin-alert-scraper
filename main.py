55d62669286d7066825364d322f94dc619040fd6
Marion Delcambre <delcambremarion@gmail.com>
2017-12-19 22:19:24 +0100

    check if file exists instead of raising exception

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #!/usr/local/bin/python
 # coding: utf-8
 
+import os
+
 import logging
 from logging.handlers import RotatingFileHandler
 
 from lxml import html
 import requests, re, time
 import email_me, utils, settings
+
+DATA_FILEPATH = '/tmp/lbc_alert_scraper'
 
 
 def create_logger():
@@ -31,9 +35,13 @@ def create_logger():
 
 if __name__ == '__main__':
 
+    if not os.path.exists(DATA_FILEPATH):
+        os.mkdir(DATA_FILEPATH)
+
     logger = create_logger()
     # Iterate over all URLS from settings
     logger.info('starting process: %s request to treat', len(settings.URLS))
+
     for url in settings.URLS :
         # Load html page
         logger.info('treating %s : %s', url, settings.URLS[url])
@@ -43,45 +51,48 @@ if __name__ == '__main__':
         # Select all <a/> elements by class name
         links_el = tree.xpath('//a[@class="list_item clearfix trackable"]')
         link_index = 0
+
+        f_path = os.path.join(DATA_FILEPATH, 'last_alert_%s.txt' % url)
+
+        if not os.path.exists(f_path):
+            logger.info('first execution: creating file')
+            open(f_path, 'a')
+            break
+
+        # Read last alert time
+        last_alert_time = utils.read_file(f_path)
         ### This algorithm is based on the date of the post
         ### Also, I had to do lots of sketchy conditions to get the element I wanted
         ### maybe there's a better way to scrap this data, feel free to improve it !
-        try:
-            # Read last alert time
-
-            last_alert_time = utils.read_file('last_alert_'+ url +'.txt')
-            for i, el in enumerate(reversed(links_el), start=0):
-                # Get date element of ad
-                date = el.xpath('.//p[@class="item_supp"]/text()')
-                # Get link element of ad
-                link = tree.xpath('.//a[@class="list_item clearfix trackable"]/@href')
-                day_hour = ''
-                for d in date:
-                    # Remove all useless char from html element
-                    day_hour = ''.join(map(lambda s: re.sub('\s+', '', s),d ))
-                if day_hour != '':
-                    # We have now someting like : <day>,<hour>:<minute>
-                    # So we split the string
-                    splitted_day_hour = day_hour.split(',')
-                    day = splitted_day_hour[0]
-                    hour = splitted_day_hour[1]
-                    if len(splitted_day_hour) == 2:
-                        # If the ad is from today
-                        if day == "Aujourd'hui":
-                            # And is not the same and newer than the last
-                            if hour != last_alert_time and hour > last_alert_time:
-                                link = link[link_index][2:-8]
-                                link_index +=1
-                                # Here I'm sending an email but you can do whatever you want, for exemple connect it to IFTTT maker channel, or send you a tweet
-                                # Send the email
-                                email_me.send_email("Alerte "+ url +" !", "Nouvelle annonce detectée à : " + time.strftime('%H:%M:%S')+".\n"+ link)
-                                # Save the new alert hour
-                                utils.write_to_file('last_alert_'+ url +'.txt', hour)
-                                logger.info("email send, new last_alert_time : %s", hour)
-                    else:
-                        logger.info('no new ad since last execution')
-        # On first execution of the script, create the last_alert.txt files
-        except FileNotFoundError:
-            open('last_alert_'+ url +'.txt', 'a')
+        for i, el in enumerate(reversed(links_el), start=0):
+            # Get date element of ad
+            date = el.xpath('.//p[@class="item_supp"]/text()')
+            # Get link element of ad
+            link = tree.xpath('.//a[@class="list_item clearfix trackable"]/@href')
+            day_hour = ''
+            for d in date:
+                # Remove all useless char from html element
+                day_hour = ''.join(map(lambda s: re.sub('\s+', '', s),d ))
+            if day_hour != '':
+                # We have now someting like : <day>,<hour>:<minute>
+                # So we split the string
+                splitted_day_hour = day_hour.split(',')
+                day = splitted_day_hour[0]
+                hour = splitted_day_hour[1]
+                if len(splitted_day_hour) == 2:
+                    # If the ad is from today
+                    if day == "Aujourd'hui":
+                        # And is not the same and newer than the last
+                        if hour != last_alert_time and hour > last_alert_time:
+                            link = link[link_index][2:-8]
+                            link_index +=1
+                            # Here I'm sending an email but you can do whatever you want, for exemple connect it to IFTTT maker channel, or send you a tweet
+                            # Send the email
+                            email_me.send_email("Alerte "+ url +" !", "Nouvelle annonce detectée à : " + time.strftime('%H:%M:%S')+".\n"+ link)
+                            # Save the new alert hour
+                            utils.write_to_file(f_path, hour)
+                            logger.info("email send, new last_alert_time : %s", hour)
+                else:
+                    logger.info('no new ad since last execution')
 
     logger.info('ending process')
