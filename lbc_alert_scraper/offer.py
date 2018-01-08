28b214acd220b1053edec3cf6b4f35f6be5287e8
Marion Delcambre <delcambremarion@gmail.com>
2018-01-08 10:48:54 +0100

    fix getting offer publish time when offer is 'Urgent'

--- lbc_alert_scraper/offer.py
+++ lbc_alert_scraper/offer.py
@@ -18,7 +18,7 @@ class Offer(object):
         info_el = self.lxml_element.xpath('a/section')[0]
         p_el = info_el.xpath('aside/p')[0]
         str_date = p_el.attrib['content']
-        str_time = p_el.xpath('text()')[0].strip().split(',')[1].strip()
+        str_time = p_el.xpath('text()')[-1].strip().split(',')[-1].strip()
 
         self.dtt_publish = datetime.strptime('%s %s' % (str_date, str_time), '%Y-%m-%d %H:%M')
         self.link = self.lxml_element.xpath('a')[0].attrib['href']
