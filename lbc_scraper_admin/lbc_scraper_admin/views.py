a6f642b3ce0a1029aeff471d532493ea7cb04799
Marion Delcambre <delcambremarion@gmail.com>
2018-02-24 21:41:04 +0100

    using class to organize views

--- lbc_scraper_admin/lbc_scraper_admin/views.py
+++ lbc_scraper_admin/lbc_scraper_admin/views.py
@@ -1,27 +1,32 @@
-from pyramid.view import view_config
+from pyramid.view import view_config, view_defaults
 from lbc_scraper.models import Recipient, Alert, Subscription
 
 
-@view_config(route_name='main', renderer='template.pt')
-def main_view(request):
-    recipients = request.dbsession.query(Recipient).all()
-    for recipient in recipients:
-        recipient.alerts = [subscription.alert for subscription in recipient.subscriptions]
-    return {'recipients': recipients}
+@view_defaults(route_name='main', renderer='template.pt')
+class AdminView(object):
 
+    def __init__(self, request):
+        self.request = request
 
-@view_config(route_name='main', renderer='template.pt', request_method='POST')
-def add_alert(request):
-    name = request.POST['name']
-    url = request.POST['url']
-    id_recipient = request.POST['id_recipient']
-    recipient = request.dbsession.query(Recipient).filter_by(id_recipient=id_recipient).one()
+    @view_config(request_method='GET')
+    def get(self):
+        recipients = self.request.dbsession.query(Recipient).all()
+        for recipient in recipients:
+            recipient.alerts = [subscription.alert for subscription in recipient.subscriptions]
+        return {'recipients': recipients}
 
-    new_alert = Alert(name=name, url=url)
-    new_subscription = Subscription(alert=new_alert, recipient=recipient)
+    @view_config(request_method='POST')
+    def post(self):
+        name = self.request.POST['name']
+        url = self.request.POST['url']
+        id_recipient = self.request.POST['id_recipient']
+        recipient = self.request.dbsession.query(Recipient).filter_by(id_recipient=id_recipient).one()
 
-    request.dbsession.add(new_subscription)
-    recipients = request.dbsession.query(Recipient).all()
-    for recipient in recipients:
-        recipient.alerts = [subscription.alert for subscription in recipient.subscriptions]
-    return {'recipients': recipients}
+        new_alert = Alert(name=name, url=url)
+        new_subscription = Subscription(alert=new_alert, recipient=recipient)
+
+        self.request.dbsession.add(new_subscription)
+        recipients = self.request.dbsession.query(Recipient).all()
+        for recipient in recipients:
+            recipient.alerts = [subscription.alert for subscription in recipient.subscriptions]
+        return {'recipients': recipients}
