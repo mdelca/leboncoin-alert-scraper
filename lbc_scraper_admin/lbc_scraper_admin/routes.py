c7f2227326fbf5240c884a303b7f5c9325f9d8d3
Marion Delcambre <delcambremarion@gmail.com>
2018-03-20 15:10:34 +0100

    on passe par une page séparée pour pouvoir ajouter une nouvelle alerte

--- lbc_scraper_admin/lbc_scraper_admin/routes.py
+++ lbc_scraper_admin/lbc_scraper_admin/routes.py
@@ -2,3 +2,4 @@ def includeme(config):
     config.add_route('users', '/lbc_scraper_admin/users')
 
     config.add_route('alerts', '/lbc_scraper_admin/users/{id_user}/alerts')
+    config.add_route('new_alert', '/lbc_scraper_admin/users/{id_user}/new_alert')
--- lbc_scraper_admin/lbc_scraper_admin/views/alerts.py
+++ lbc_scraper_admin/lbc_scraper_admin/views/alerts.py
@@ -1,5 +1,6 @@
 import logging
 
+from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config, view_defaults
 
 from lbc_scraper.models import Alert, Subscription, Recipient
@@ -23,16 +24,6 @@ class AlertView(object):
         self.logger.info('%s alerts available for user %s', len(alerts), recipient.name)
         return {'alerts': alerts, 'recipient': recipient, 'message': message}
 
-    def add_new_alert(self, name, url, id_recipient):
-        self.logger.info('request : add new alert (%s) for user %s', url, id_recipient)
-
-        recipient = self.request.dbsession.query(Recipient).filter_by(id_recipient=id_recipient).one()
-
-        new_alert = Alert(name=name, url=url)
-        new_subscription = Subscription(alert=new_alert, recipient=recipient)
-
-        self.request.dbsession.add(new_subscription)
-
     def delete_alert(self, id_alert):
         self.logger.info('request : delete alert %s', id_alert)
         alert = self.request.dbsession.query(Alert).filter_by(id_alert=id_alert).one()
@@ -44,21 +35,53 @@ class AlertView(object):
 
     @view_config(request_method='POST')
     def post(self):
+
+        if 'delete' in self.request.params:
+            id_alert = self.request.POST['id_alert']
+            self.delete_alert(id_alert)
+        return self.get_alerts()
+
+
+@view_defaults(route_name='new_alert', renderer='../templates/new_alert.pt')
+class NewAlertView(object):
+
+    def __init__(self, request):
+        self.request = request
+        self.logger = logging.getLogger()
+
+    def get_new_alert_form(self, message=None):
+        id_recipient = int(self.request.matchdict['id_user'])
+        recipient = self.request.dbsession.query(Recipient).filter_by(id_recipient=id_recipient).one()
+        return {'recipient': recipient, 'message': message}
+
+    @view_config(request_method='GET')
+    def get(self):
+        return self.get_new_alert_form()
+
+    @view_config(request_method='POST')
+    def post(self):
         if 'add' in self.request.params:
             name = self.request.POST['name'].strip()
             url = self.request.POST['url'].strip()
             id_recipient = int(self.request.matchdict['id_user'])
 
             if not (url or name):
-                return self.get_alerts(message="Tous les champs sont obligatoires")
+                return self.get_new_alert_form(message="Tous les champs sont obligatoires")
 
             result = check_url(url)
             if not result.is_valid:
-                return self.get_alerts(message=result.message)
+                return self.get_new_alert_form(message=result.message)
 
             self.add_new_alert(name, url, id_recipient)
 
-        if 'delete' in self.request.params:
-            id_alert = self.request.POST['id_alert']
-            self.delete_alert(id_alert)
-        return self.get_alerts()
+        return HTTPFound(location=self.request.route_url('alerts', id_user=id_recipient))
+
+    def add_new_alert(self, name, url, id_recipient):
+        self.logger.info('request : add new alert (%s) for user %s', url, id_recipient)
+
+        recipient = self.request.dbsession.query(Recipient).filter_by(id_recipient=id_recipient).one()
+
+        new_alert = Alert(name=name, url=url)
+        new_subscription = Subscription(alert=new_alert, recipient=recipient)
+
+        self.request.dbsession.add(new_subscription)
